4e4a369cd4763b1c78e30fe72dd056dbf56f2e19
Om Jadav <omjadav04@gmail.com>
2024-05-13 22:40:02 +0530

    updated for deploy 1

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv, { config } from 'dotenv';
 import process from 'process'
 import cookieParser from 'cookie-parser'
 import connectDB from './config/db.js';
+import cors from 'cors'
 dotenv.config();
 import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middlewares/errorMiddleware.js';
@@ -11,25 +12,19 @@ const port = process.env.PORT;
 connectDB();
 const app = express();
 
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, 'frontend/dist')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
+}
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-    const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, 'frontend/dist')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-    });
-    // app.use(express.static(path.join(__dirname, "dist")))
-    // app.get("*", (req, res) => {
-    //     res.sendFile(path.join(__dirname, "dist", "index.html"))
-    // })
-} else {
-    app.get('/', (req, res) => res.send('Server is Ready'))
-}
+
 
 app.use(notFound);
 app.use(errorHandler);
