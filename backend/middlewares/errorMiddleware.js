3ee4b6efefc5164f9ef9661f681e682980c3bc01
Om Jadav <omjadav04@gmail.com>
2024-05-13 23:11:34 +0530

    update 4

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -13,8 +13,7 @@ const errorHandler = (err, req, res, next) => {
 
     }
     res.status(statusCode).json({
-        message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+        message
     })
 }
 
